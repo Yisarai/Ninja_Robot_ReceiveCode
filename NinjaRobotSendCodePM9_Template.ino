cdd00c20d08fd0a4cc3b2d23dd086c5d97b9f315
Yisarai <38140179+Yisarai@users.noreply.github.com>
2018-04-06 11:37:30 -0600

    Add files via upload

---
+++ NinjaRobotSendCodePM9_Template.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+char userInput;
+
+SoftwareSerial mySerial(2,3); // RX, TX
+
+void setup() {
+  // Open serial communications with computer and wait for port to open:
+  Serial.begin(9600);
+
+  // Open serial communications with the other Arduino board
+  mySerial.begin(9600);
+}
+ void loop (){
+   if (Serial.available()) {
+    userInput = Serial.read();
+    Serial.print("The user entered ");
+    Serial.println(userInput);
+    mySerial.write(Serial.read());
+    delay(50);
+ }
+ }
+
