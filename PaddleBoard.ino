49f7f4c0e4bdb5df44469ffc76d7481baf4c2f29
Jared Christensen <jaredchristensen17@gmail.com>
2018-04-06 11:42:39 -0600

    Add files via upload

---
+++ PaddleBoard.ino
@@ -0,0 +1,7 @@
+Encoder myEnc1(20,21);  
+Encoder myEnc2(18,19);  //create instances for each encoder
+float GearRatio = 131;            //the gear ratio
+int countsPerRev_motor = 64;      //the counts per revolution of the motor shaft
+int counts1 = 0;                   //Globally intialize encoder counts
+int counts2 = 0;                   //Globally intialize encoder counts
+int f = 2;      
