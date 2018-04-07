f3a620d2707c9dacf10cc1e035b684f0c983c818
Jared Christensen <jaredchristensen17@gmail.com>
2018-04-07 13:35:41 -0600

    Add files via upload

--- RailRunner.ino
+++ RailRunner.ino
@@ -1,5 +1,13 @@
 void RailRunner(){
-
-  
+  FrontRangeFinder();
+  while (lineLoc < -3)
+  {
+    md.setM3Speed(250);
+    if (sensorDistance < 5)
+    {
+      Serial.println("Drive Straight");
+      PIDControl(100,100,10*PI,10*PI)
+    };
+  };
 }
 
