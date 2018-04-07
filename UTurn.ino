37936c0c621493e7e47f41ff99a2757a7bd85dc7
nedchanatry <38164919+nedchanatry@users.noreply.github.com>
2018-04-07 13:46:24 -0600

    Add files via upload

--- UTurn.ino
+++ UTurn.ino
@@ -1,4 +1,12 @@
 void UTurn() {
-  LineFollow();
-}
+  // while line sensort dectects line, run LineFollow code
 
+FrontRangeFinder();
+while (sensorDistance =<5 || lineLoc >= -3 ){   // front sensort is above floor or line is detected
+
+StepperMotor(96);   //set stepper angle from ground (-14) to 85 degrees
+
+ LineFollow();   // run line follow
+ 
+  }
+}
